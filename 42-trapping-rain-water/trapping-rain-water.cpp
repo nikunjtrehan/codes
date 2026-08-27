450aecb746cde4f4880cd404f50b368b9475fec1
Nikunj Trehan <ntrehan_be24@thapar.edu>
2026-08-27 11:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.52%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+      int total=0;
+      int n=height.size();
+      vector<int>leftmax(n);
+      vector<int>rightmax(n);
+      leftmax[0]=height[0];
+      for(int i=1;i<n;i++){
+          
+          if(height[i]>leftmax[i-1])leftmax[i]=height[i];
+          else{
+              leftmax[i]=leftmax[i-1];
+          }
+      }
+      
+            if(height[n-1]) { rightmax[n-1]=height[n-1];}
+      for(int i=n-2;i>=0;i--){
+         
+          if(height[i]>rightmax[i+1])rightmax[i]=height[i];
+          else{
+              rightmax[i]=rightmax[i+1];
+          }
+      }
+        
+        
+        for(int i=0;i<n;i++){
+            total+=min(leftmax[i],rightmax[i])-height[i];
+        }
+        return total;
+    }
+
+
+};
